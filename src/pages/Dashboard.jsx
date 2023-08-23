f8e4147ffc6b7b0cf30899bb9c05b25ebcd417f4
gabri <gabriellondero40@gmail.com>
2023-08-23 07:36:02 -0300

    creacion del dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,20 @@
-
+import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className="dashboard_container">
+      <Nav />
+      <div className="dashboard_main">
+        <section className="header_events">
+          <h1 style={{ fontSize: "30px" }}>Your Events</h1>
+          <Link to="/create/event" className="link">
+            Create new event
+          </Link>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
-
+export default Dashboard;
