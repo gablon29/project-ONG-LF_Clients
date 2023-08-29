78a169572d79607126cc433773c84b8cd772aacd
gabri <gabriellondero40@gmail.com>
2023-08-29 07:16:57 -0300

    terminando el componente de EventDetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import Events from "./pages/Events"
-import EventCategories from "./pages/EventCategories"
-import CreateEvent from "./pages/CreateEvent"
-import EventDetails from "./pages/EventDetails"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Events from "./pages/Events";
+import EventCategories from "./pages/EventCategories";
+import CreateEvent from "./pages/CreateEvent";
+import EventDetails from "./pages/EventDetails";
 
 function App() {
   return (
@@ -19,9 +18,7 @@ function App() {
         <Route path="/event/:slug" element={<EventDetails />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
